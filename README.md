9346200f7fdb40a2801e8426209568fa9a9a4756
eray-onur <eray.onur.developer@gmail.com>
2022-01-02 02:30:10 +0300

    Fully implemented converter api

--- README.md
+++ README.md
@@ -15,7 +15,3 @@ Drop all image assets to be converted inside 'assets/unoptimized' folder and run
 ## How to deploy this project as a server?
 
 Running the command `npm run start` does the trick. It's also possible to deploy in CI/CD services such as Netlify, Heroku, etc. with no hassle at all! Just fork the repository and connect the repo with any service you'd like.
-
-## Planned features:
-
-* HTTP/HTTPS Service feature,
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
+require('dotenv').config();
+
+
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
 ffmpeg.setFfmpegPath(ffmpegPath);
+
+const { pipeline } = require('stream');
+const util = require('util');
+const pump = util.promisify(pipeline);
 
 const fs = require('fs');
 
 const assetDir = './assets/unoptimized';
 const optimizedAssetsDir = `${assetDir}/optimized`;
 
+// fastify initialization
 const fastify = require('fastify')({
     logger: true
 });
 
+// form-data middleware
+fastify.register(require('fastify-multipart'), {
 
+});
+
+// Controller for serving an already stored file as webp.
 fastify.get('/*', async(request, reply) => {
     const url = request.url.slice(1);
     const fileName = url.substring(0, url.indexOf('.'));
@@ -42,9 +55,32 @@ fastify.get('/*', async(request, reply) => {
         .send({'message': 'Asset not found!'});
 });
 
+// Controller for handling conversions of uploaded file.
+fastify.post('/*', async(request, reply) => {
+    try {
+        const data = await request.file();
+        const filePath = `${process.env.UNOPTIMIZED_ASSETS_PATH}/${data.filename}`;
+        await pump(data.file, fs.createWriteStream(filePath))
+        const newFileName = data.filename.split('.')[0];
+
+        // convert file
+        ffmpeg().input(`${process.env.UNOPTIMIZED_ASSETS_PATH}/${data.filename}`)
+        .saveToFile(`${process.env.OPTIMIZED_ASSETS_PATH}/${newFileName}.webp`);
+
+        reply.header('Content-Disposition', `attachment; filename = ${newFileName}.webp`);
+        reply.header('Content-Length', data.length);
+        reply.type('image/webp');
+        reply.send(fs.readFileSync(`${process.env.OPTIMIZED_ASSETS_PATH}/${newFileName}.webp`));
+
+    } catch(ex) {
+        throw Error('Failed to convert image.');
+    }
+
+});
+
 const start = async () => {
     try {
-        await fastify.listen(3000);
+        await fastify.listen(3009);
     } catch(err) {
         fastify.log.error(err);
         process.exit(1);
